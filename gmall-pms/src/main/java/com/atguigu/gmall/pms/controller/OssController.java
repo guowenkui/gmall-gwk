f469c5e063656dbf9c7eeea65637e65fa0e9a87b
郭文魁 <guowenkui@rongzi.com>
2020-03-10 10:14:48 +0800

    图片上传-阿里OSS

---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/controller/OssController.java
@@ -0,0 +1,80 @@
+package com.atguigu.gmall.pms.controller;
+
+
+import com.alibaba.fastjson.JSONObject;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.common.utils.BinaryUtil;
+import com.aliyun.oss.model.MatchMode;
+import com.aliyun.oss.model.PolicyConditions;
+import com.atguigu.core.bean.Resp;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("pms/oss")
+public class OssController {
+
+    @GetMapping("policy")
+    public Resp<Object> policy(){
+
+        String accessId = "LTAI4FdW62o1Cayvx2NEM12x"; // 请填写您的AccessKeyId。
+        String accessKey = "4jj7emi0QD6ruSzsEgp20gToBA5iV0"; // 请填写您的AccessKeySecret。
+        String endpoint = "oss-cn-beijing.aliyuncs.com"; // 请填写您的 endpoint。
+        String bucket = "ggmall-gwk"; // 请填写您的 bucketname 。
+        String host = "https://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
+        // callbackUrl为 上传回调服务器的URL，请将下面的IP和Port配置为您自己的真实信息。
+//        String callbackUrl = "http://88.88.88.88:8888";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String dir = simpleDateFormat.format(new Date()); // 用户上传文件时指定的前缀。
+
+        OSSClient client = new OSSClient(endpoint, accessId, accessKey);
+        try {
+            long expireTime = 30;
+            long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
+            Date expiration = new Date(expireEndTime);
+            PolicyConditions policyConds = new PolicyConditions();
+            policyConds.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, 1048576000);
+            policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
+
+            String postPolicy = client.generatePostPolicy(expiration, policyConds);
+            byte[] binaryData = postPolicy.getBytes("utf-8");
+            String encodedPolicy = BinaryUtil.toBase64String(binaryData);
+            String postSignature = client.calculatePostSignature(postPolicy);
+
+            Map<String, String> respMap = new LinkedHashMap<String, String>();
+            respMap.put("accessid", accessId);
+            respMap.put("policy", encodedPolicy);
+            respMap.put("signature", postSignature);
+            respMap.put("dir", dir);
+            respMap.put("host", host);
+            respMap.put("expire", String.valueOf(expireEndTime / 1000));
+            // respMap.put("expire", formatISO8601Date(expiration));
+
+//            JSONObject jasonCallback = new JSONObject();
+//            jasonCallback.put("callbackUrl", callbackUrl);
+//            jasonCallback.put("callbackBody",
+//                    "filename=${object}&size=${size}&mimeType=${mimeType}&height=${imageInfo.height}&width=${imageInfo.width}");
+//            jasonCallback.put("callbackBodyType", "application/x-www-form-urlencoded");
+//            String base64CallbackBody = BinaryUtil.toBase64String(jasonCallback.toString().getBytes());
+//            respMap.put("callback", base64CallbackBody);
+//
+//            JSONObject ja1 = JSONObject.fromObject(respMap);
+//            // System.out.println(ja1.toString());
+//            response.setHeader("Access-Control-Allow-Origin", "*");
+//            response.setHeader("Access-Control-Allow-Methods", "GET, POST");
+//            response(request, response, ja1.toString());
+
+            return Resp.ok(respMap);
+        } catch (Exception e) {
+            // Assert.fail(e.getMessage());
+            System.out.println(e.getMessage());
+        }
+        return Resp.ok(null);
+    }
+}
