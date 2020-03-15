5d46e296678844b9c76b694f0f8165c3636a92e6
郭文魁 <guowenkui@rongzi.com>
2020-03-15 10:53:24 +0800

    保存spu相关数据,保存sku相关数据

--- gmall-pms/src/main/java/com/atguigu/gmall/pms/vo/SpuInfoVO.java
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/vo/SpuInfoVO.java
@@ -11,5 +11,5 @@ public class SpuInfoVO  extends SpuInfoEntity {
 
     private List<String> spuImages;
     private List<BaseAttrVO> baseAttrs;
-    private List<?> skus;
+    private List<SkuInfoVO> skus;
 }
