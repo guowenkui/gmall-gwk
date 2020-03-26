c2cf29aa9e7fd0ee110884786c68af84eb76db43
郭文魁 <guowenkui@rongzi.com>
2020-03-26 16:14:23 +0800

    封装搜索参数

---
+++ gmall-search/src/main/java/com/atguigu/gmall/search/service/SearchService.java
@@ -0,0 +1,12 @@
+package com.atguigu.gmall.search.service;
+
+
+import com.atguigu.gmall.search.pojo.SearchParam;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SearchService {
+
+    public void search(SearchParam searchParam) {
+    }
+}
